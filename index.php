e9ff76176b25acf13aeabc8dc27c4d1f3bce9d83
RobertoFilho0100 <robertocorreiafilho0100@gmail.com>
2024-12-01 22:14:03 -0300

    criando index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estruturas de Repetição</title>
+</head>
+<body>
+    <h1>Estruturas de Repetição</h1>
+    <ul>
+        <li>
+            <a href="for.php">For</a>
+        </li>
+        <li>
+            <a href="foreach.php">For</a>
+        </li>
+        <li>
+            <a href="while.php">For</a>
+        </li>
+        <li>
+            <a href="do-while.php">For</a>
+        </li>
+    </ul>
+</body>
+</html>
