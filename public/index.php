3ff24da835510ef4e8d05891d726c078310dfe03
RobertoFilho0100 <robertocorreiafilho0100@gmail.com>
2024-12-19 16:35:53 -0300

    feat: adiciona helper dd

--- public/index.php
+++ public/index.php
@@ -4,12 +4,4 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use App\Models\Product;
-
-$title = 'My WebStore';
-$heading = 'Home';
-
-$products = Product::all();
-
-
-require __DIR__ . '/../resources/views/index.php';
+dd($_SERVER);
