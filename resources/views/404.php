e6c7c3ffddc6b5b5bcdc4fc5004b7dcd248d3402
RobertoFilho0100 <robertocorreiafilho0100@gmail.com>
2024-12-20 15:13:08 -0300

    feat: adiciona view para http code 404

--- resources/views/404.php
+++ resources/views/404.php
@@ -1,10 +1,15 @@
 <?php require 'includes/head.php' ?>
 <?php require 'includes/top-nav.php' ?>
-<?php require 'includes/header.php' ?>
 
 <main>
     <div class="mx-auto max-w-7xl py-6 px-8">
-        My main content here
+        <h1 class="text-3xl leading-6 font-bold">
+            404 - Not Found
+        </h1>
+
+        <div class="mt-10">
+            <a href="/">Back to Home</a>
+        </div>
     </div>
 </main>
 
