f4f50d42761eaa65320ce90236fa7d3bc04e2e14
RobertoFilho0100 <robertocorreiafilho0100@gmail.com>
2024-12-20 15:12:31 -0300

    feat: adiciona hash table para routing

--- public/index.php
+++ public/index.php
@@ -4,4 +4,17 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-dd($_SERVER);
+$uri = $_SERVER['REQUEST_URI'];
+
+$routes = [
+    '/' => 'home',
+    '/contact' => 'contact',
+];
+
+if (array_key_exists($uri, $routes)) {
+    require base_path('src/Controllers/' . $routes[$uri] . '.php');
+} else {
+    http_response_code(404);
+    require resource_path('views/404.php');
+    die();
+}
