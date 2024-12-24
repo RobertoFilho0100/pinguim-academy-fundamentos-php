c99e6d20b4fead35b2ea7ead1ef7638e84a4d977
RobertoFilho0100 <robertocorreiafilho0100@gmail.com>
2024-12-23 21:59:57 -0300

    feat: adiciona pdo para obtenção dos produtos.

--- src/Controllers/home.php
+++ src/Controllers/home.php
@@ -5,4 +5,11 @@ declare(strict_types=1);
 $title = 'My Webstore';
 $heading = 'Home';
 
+$pdo = (new \Core\Database\Connector())->connect();
+
+$statement = $pdo->prepare('SELECT * FROM products;');
+$statement->execute();
+
+$products = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
 require resource_path('views/index.php');
