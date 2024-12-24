63fc66ae3e87d935e12292e3c91f970f0656732b
RobertoFilho0100 <robertocorreiafilho0100@gmail.com>
2024-12-23 21:59:41 -0300

    feat: remove obtenção dos produtos do index para ser utilizado no controller.

--- resources/views/index.php
+++ resources/views/index.php
@@ -6,6 +6,19 @@
     <div class="mx-auto max-w-7xl py-6 px-8">
         My main content here
     </div>
+
+    <div>
+        <h2>Products</h2>
+        <ul>
+            <?php foreach ($products as $product): ?>
+                <li>
+                    <a href="/products?id=<?= $product['id'] ?>">
+                        <?= $product['name'] ?> - R$<?= $product['price'] ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
 </main>
 
 <?php require 'includes/footer.php' ?>
